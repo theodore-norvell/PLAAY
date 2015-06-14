37f25dfe6e5ea4731426669d578b8a912285ac81
Dillon Butt <dillonbutt@gmail.com>
2015-06-14 17:32:57 -0230

    Hello World
    - Initial Staging for PLAAY Environment

--- haxe/src/Test.hx
+++ haxe/src/Test.hx
@@ -1,11 +1,18 @@
 package ;
+import js.Browser;
 class Test {
     public function new() {
 
     }
 
-    public static function main()
+    public static function main ()
     {
-        trace("Hello World!");
+        var win = Browser.window;
+        var document = win.document;
+        var pEl = document.createElement("p");
+        var text = document.createTextNode("Hello World via JavaScript");
+        pEl.appendChild(text);
+        //And we can add it to the document by doing:
+        document.body.appendChild(pEl);
     }
 }
