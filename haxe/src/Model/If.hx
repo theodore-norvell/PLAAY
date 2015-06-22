d64bbb12de1576d28bd55f72781946b21e6f12e8
Dillon Butt <dillonbutt@gmail.com>
2015-06-22 13:12:59 -0230

    If file first iteration

--- haxe/src/Model/If.hx
+++ haxe/src/Model/If.hx
@@ -1,31 +1,35 @@
 package model;
 
-class If extends Expression{
+class If {
     public function new()
     {
         //Pass expressions and store here TODO
     }
 
-    public function If(functionName:Array<String>,logic:Array<Expression>,expression:Array<Expression>):Expression
-    {
-        if(functionName[0]=="If" || functionName[0]=="Else If")
-        {
-            if(logic[0])
-            {
-                return expression[0];
-            }
-            else
-            {
-                if(functionName.length>1)
-                {
-                    If(functionName.slice(1),logic.slice(1),expression.slice(1));
-                }
-            }
-        }
-        else
-        {
-            return expression[0];
-        }
-    }
+//    public function If(functionName:Array<String>,logic:Array<Expression>,expression:Array<Expression>):Expression
+//    {
+//        if(functionName[0]=="If" || functionName[0]=="Else If")
+//        {
+//            if(logic[0])
+//            {
+//                return expression[0];
+//            }
+//            else
+//            {
+//                if(functionName.length>1)
+//                {
+//                    return If(functionName.slice(1),logic.slice(1),expression.slice(1));
+//                }
+//            }
+//        }
+//        else
+//        {
+//            return expression[0];
+//        }
+//    }
 
+    public function If()
+    {
+
+    }
 }
