a674ce9ceb3051ab72b6a7e8a19d63da374f6b4c
jasoncwhowell <jasoncwhowell@gmail.com>
2016-01-25 14:15:46 -0330

    create a default screen

--- typescriptSrc/mkHTML.ts
+++ typescriptSrc/mkHTML.ts
@@ -9,22 +9,48 @@ module mkHTML {
     import List = collections.List;
     
     export function onLoad() : void {
-        const div = document.createElement( "div" ) ;
-        div.setAttribute( "class", "H" ) ;
-        div["childCount"] = 3 ;
-        const text = document.createTextNode( "Hello world. I am child "
-                                           + div["childCount"] ) ;
-        div.appendChild( text ) ;
-        var body = document.getElementById( "body" ) ;
-        body.appendChild( div ) ;
-        
-        
-        const l : List<string> = list( "This", "is", "more", "text" ) ;
-        l.map( ( str : string ) => {
-                       const p = document.createElement( "p" ) ;
-                       const t = document.createTextNode( str ) ;
-                       p.appendChild( t ) ;
-                       body.appendChild( p ) ; } ) ;
+        //creates side bar
+        const sidebar = document.createElement("div");
+        sidebar.setAttribute("id","sidebar");
+        sidebar.setAttribute("class","sidebar");
+        document.getElementById("body").appendChild(sidebar);
+
+        //create elements
+        //can't move div's yet so we will use images as a placeholder
+        const ifblock = document.createElement("div");
+        ifblock.setAttribute("id","if");
+        ifblock.setAttribute("class","ifBox V");
+        ifblock.setAttribute("draggable","true");
+        ifblock.setAttribute("ondragstart","drag(event)");
+        document.getElementById("sidebar").appendChild(ifblock);
+
+        //creates container for code
+        const container = document.createElement("div");
+        container.setAttribute("id","container");
+        container.setAttribute("class", "container");
+        document.getElementById("body").appendChild(container);
+
+        //creates empty dropzone
+        const div = document.createElement("div") ;
+        div.setAttribute("class", "dropZone H") ;
+        div.setAttribute("ondrop", "drop(event)");
+        div.setAttribute("ondragover","allowDrop(event)");
+        div["childCount"] = 0 ;
+        document.getElementById("container").appendChild( div ) ;
+    }
+
+    export function allowDrop(ev) {
+        ev.preventDefault();
+    }
+
+    export function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+    }
+
+    export function drop(ev) {
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("text");
+        ev.target.appendChild(document.getElementById(data));
     }
 }
 
