5cc030f9b0d9241572d544cda1cebbe7be9e0813
Theodore Norvell <theo@mun.ca>
2016-03-28 17:47:51 -0230

    Made a simple turtle microworld.

---
+++ typescriptSrc/seymour.ts
@@ -0,0 +1,117 @@
+
+
+module seymour {
+    class Point {   
+        private _x : number = 0 ;
+        private _y : number = 0 ;
+        constructor( x : number, y : number) { this._x = x ; this._y = y ; }
+        
+        public x() { return this._x ; }
+        public y() { return this._y ; }
+    }
+    interface Segment {   
+        p0 : Point ;
+        p1 : Point ;
+    }
+    export class TurtleWorld {
+        // Defining the world to view mapping
+        private zoom : number = 1 ;
+        private worldWidth : number = 100 ;
+        private worldHeight : number = 100 ;
+        
+        // The turtle
+        private posn : Point = new Point(0,0) ;
+        // Invariant: The orientation is in [0,360)
+        private orientation : number = 0.0 ;
+        private visible = true ;
+        private penIsDown = false ;
+        
+        // The segments 
+        private segments = new Array<Segment>() ;
+        
+        // The canvas
+        private canv : HTMLCanvasElement = document.createElement('canvas');
+        
+        getCanvas() { return this.canv ; }
+        
+        forward( n : number ) {
+            const theta = this.orientation / 180.0 * Math.PI ;
+            const newx =this.posn.x() + n * Math.cos(theta) ;
+            const newy =this.posn.y() + n * Math.sin(theta) ;
+            const newPosn = new Point(newx, newy) ;
+            if( this.penIsDown ) { this.segments.push( {p0 : this.posn, p1:newPosn})} ;
+            this.posn = newPosn ;
+            this.redraw() ;
+        }
+        
+        clear() { 
+            this.segments = new Array<Segment>() ;
+        }
+        
+        right( d : number ) { 
+            var r = (this.orientation + d) % 360 ;
+            while( r < 0 ) r += 360 ; // Once should be enough. Note that if r == -0 to start then it equals +360 to end!
+            while( r >= 360 ) r -= 360 ; // Once should be enough.
+            this.orientation = r ;
+            this.redraw() ;
+         }
+        
+        left( d : number ) { 
+            this.right( - d ) ;
+         }
+         
+         penUp() { this.penIsDown = false ; }
+         
+         penDown() { this.penIsDown = true ; }
+         
+         hide() { this.visible = false ; this.redraw() ; }
+         
+         show() { this.visible = true ; this.redraw() ;  }
+         
+         redraw() {
+             const ctx = this.canv.getContext("2d") ;
+             const w = this.canv.width ;
+             const h = this.canv.height ;
+             ctx.clearRect(0, 0, w, h);
+             for( let i = 0 ; i < this.segments.length ; ++i ) {
+                 const p0v = this.world2View( this.segments[i].p0, w, h ) ;
+                 const p1v = this.world2View( this.segments[i].p1, w, h ) ;
+                 ctx.beginPath() ;
+                 ctx.moveTo( p0v.x(), p0v.y() ) ;
+                 ctx.lineTo( p1v.x(), p1v.y() ) ;
+                 ctx.stroke() ;
+             }
+             if( this.visible ) {
+                 // Draw a little triangle
+                 const theta = this.orientation / 180.0 * Math.PI ;
+                 const x = this.posn.x() ;
+                 const y = this.posn.y() ;
+                 const p0x = x + 4 *  Math.cos(theta) ;
+                 const p0y = y + 4 *  Math.sin(theta) ;
+                 const p1x = x + 5 * Math.cos(theta+2.5) ;
+                 const p1y = y + 5 * Math.sin(theta+2.5) ;
+                 const p2x = x + 5 * Math.cos(theta-2.5) ;
+                 const p2y = y + 5 * Math.sin(theta-2.5) ;
+                 const p0v = this.world2View( new Point(p0x,p0y), w, h ) ;
+                 const p1v = this.world2View( new Point(p1x,p1y), w, h ) ;
+                 const p2v = this.world2View( new Point(p2x,p2y), w, h ) ;
+                 ctx.beginPath() ;
+                 ctx.moveTo(p0v.x(),p0v.y()) ;
+                 ctx.lineTo(p1v.x(),p1v.y()) ;
+                 ctx.lineTo(p2v.x(),p2v.y()) ;
+                 ctx.lineTo(p0v.x(),p0v.y()) ;
+                 ctx.stroke() ;
+             }
+         }
+         
+         private world2View( p : Point, viewWidth : number, viewHeight : number ) {
+             const hscale = viewWidth / this.worldWidth * this.zoom ;
+             const vscale = viewHeight / this.worldHeight * this.zoom ;
+             const x = p.x() * hscale + viewWidth/2 ;
+             const y = p.y() * vscale + viewHeight/2 ;
+             return new Point( x, y ) ;
+         }
+    }
+}
+
+export = seymour ;
---
+++ typescriptSrc/turtleTest.ts
@@ -0,0 +1,48 @@
+/// <reference path="seymour.ts" />
+
+import seymour = require( './seymour' ) ;
+
+function turtleTest() {
+    console.log("running") ;
+    const body = document.getElementById('body') ;
+    const turtleWorld = new seymour.TurtleWorld() 
+    const canv = turtleWorld.getCanvas();
+    canv.setAttribute('width','200') ;
+    canv.setAttribute('height','200') ;
+    body.appendChild( canv ) ;
+    
+    let playList : Array< () => void > = [
+        () => {turtleWorld.penDown()},
+        () => {turtleWorld.forward(20)},
+        () => {turtleWorld.right(90)},
+        () => {turtleWorld.forward(20)},
+        () => {turtleWorld.right(90)},
+        () => {turtleWorld.forward(20)},
+        () => {turtleWorld.right(90)},
+        () => {turtleWorld.forward(20)},
+        () => {turtleWorld.right(90)},
+        () => {turtleWorld.clear()},
+        () => {turtleWorld.hide()},
+        () => {turtleWorld.penUp()},       
+        () => {turtleWorld.forward(-20)},
+        () => {turtleWorld.show()},
+        () => {turtleWorld.right(90)},  
+        () => {turtleWorld.forward(10)}, 
+        () => {turtleWorld.penDown()},
+        
+        () => {for(let i=0 ; i< 360 ; ++i) {turtleWorld.forward(0.2) ; turtleWorld.left(1) ; } },
+        
+        () => {turtleWorld.hide()}
+    ] ;
+    
+    function playFrom( i : number ) {
+        if( i < playList.length ) {
+            playList[i]() ;
+            setTimeout( ()=> playFrom(i+1), 500 ) ;
+        }
+    }
+    
+    setTimeout( () => playFrom(0), 0 ) ;
+}
+
+export = turtleTest ;
