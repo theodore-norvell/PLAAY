fcf022e67c2fc329351b386a1a619fb4186d5bf2
Theodore Norvell <theo@mun.ca>
2016-06-29 14:22:07 -0230

    Made assert module more useful. It now prints stack traces and pops up an alert, when in the browser.

--- typescriptSrc/assert.ts
+++ typescriptSrc/assert.ts
@@ -1,8 +1,38 @@
 module assert {
+    var doAlert = true ;
+    if(doAlert) try{eval('alert'); doAlert=true;} catch(e) {doAlert = false;}
+
+    function raiseTheAlarm( message : string ) {
+        const err = new Error( message ) ;
+        if( console.error !== undefined && err['stack'] !== undefined ) {
+            console.error( err['stack'] ) ; }
+        if( doAlert ) alert( message ) ;
+        throw err ;
+    }
+
     export function check( b : Boolean, message? : string ) {
         if( !b ) {
             if( message===undefined ) message = "Assertion failed" ;
-            throw new Error( message ) ; } }
+            else message = "Assertion failed: "+message ;
+            raiseTheAlarm( message ) ;
+        }
+    }
+
+    export function checkPrecondition( b : Boolean, message? : string ) {
+        if( !b ) {
+            if( message===undefined ) message = "Precondition failed" ;
+            else message = "Precondition failed: "+message ;
+            raiseTheAlarm( message ) ;
+        }
+    }
+
+    export function checkInvariant( b : Boolean, message? : string ) {
+        if( !b ) {
+            if( message===undefined ) message = "Invariant failed" ;
+            else message = "Invariant failed: "+message ;
+            raiseTheAlarm( message ) ;
+        }
+    }
 }
 
 export = assert ;
