7b820802d74c1dec34d628903e5668bad5698ccb
Cem Kilic <cem1kilic@gmail.com>
2016-07-22 18:28:10 -0230

    Type annotations

--- typescriptSrc/mkHTML.ts
+++ typescriptSrc/mkHTML.ts
@@ -56,45 +56,16 @@ module mkHTML {
     var turtle : boolean = false ;
     currentSelection = select;
 
-	function create(elementType, className, idName, parentElement) {
-		var obj = $("<" + elementType + "></" + elementType + ">");
-		if (className) { obj.addClass(className); }
-		if (idName) { obj.attr("id", idName); }
-		if (parentElement) { obj.appendTo(parentElement); }
-		return obj;
-	}
-	
-	function createTexted(elementType, className, idName, parentElement, textContent) {
-		var obj = create(elementType, className, idName, parentElement);
-		if (textContent) { obj.text(textContent); }
-		return obj;
-	}
-
-	function createValued(elementType, parentElement, value) {
-		var obj = $("<" + elementType + "></" + elementType + ">");
-		if (parentElement) { obj.appendTo(parentElement); }
-		if (value) { obj.val(value); }
-		return obj;
-	}
-
-	function createPrepended(elementType, className, idName, parentElement, textContent, prependToThis) {
-		var obj = createTexted(elementType, className, idName, parentElement, textContent);
-		if (prependToThis) { obj.prependTo(prependToThis); }
-		return obj;
-	}
-
-	function createHidden(elementType, className, idName, parentElement, textContent) {
-		return createTexted(elementType, className, idName, parentElement, textContent).css("visibility", "hidden");
-	}
-
     export function onLoad() : void
     {
-		create("div", "sidebar evalHidden", "sidebar", "body");
+		const bodyConst = $("body");
+		create("div", "sidebar evalHidden", "sidebar", bodyConst);
 		const sidebarConst = $("#sidebar");
-		createHidden("div", "stack evalVisible", $("#stackbar"), "body", null);
+		
+		createHidden("div", "stack evalVisible", "stackbar", bodyConst, null);
 		create("table", null, "stackVal", $("#stackbar"));
 
-		createTexted("div", "undo evalHidden", "undo", "body", "Undo");
+		createTexted("div", "undo evalHidden", "undo", bodyConst, "Undo");
         $("#undo").click(function() {
 			if (undostack.length != 0) {
 				redostack.push(currentSelection);
@@ -104,7 +75,7 @@ module mkHTML {
 			}
 		});
 
-		createTexted("div", "redo evalHidden", "redo", "body", "Redo");
+		createTexted("div", "redo evalHidden", "redo", bodyConst, "Redo");
         $("#redo").click(function() {
 			if (redostack.length != 0) {
                 undostack.push(currentSelection);
@@ -114,29 +85,30 @@ module mkHTML {
             }
 		});
 
-		createTexted("div", "play evalHidden", "play", "body", "Play").click(function() {evaluate();});
-		createTexted("div", "turtle", "turtle", "body", "Turtle World").click(function() {turtleGraphics();});
-		createHidden("div", "quitworld", "quitworld", "body", "Quit World").click(function() {leaveWorld();});
-		createHidden("div", "edit evalVisible", "edit", "body", "Edit").click(function() {editor();});
-		createTexted("div", "trash evalHidden", "trash", "body", "Trash").click(function() {visualizeTrash();});
-		createHidden("div", "advance evalVisible", "advance", "body", "Next").click(function() {advanceOneStep();});
-		createHidden("div", "multistep evalVisible", "multistep", "body", "Multi-Step").click(function() {multiStep();});
-		createHidden("div", "run evalVisible", "run", "body", "Run").click(function() {stepTillDone();});
+		createTexted("div", "play evalHidden", "play", bodyConst, "Play").click(function() {evaluate();});
+		createTexted("div", "turtle", "turtle", bodyConst, "Turtle World").click(function() {turtleGraphics();});
+		createHidden("div", "quitworld", "quitworld", bodyConst, "Quit World").click(function() {leaveWorld();});
+		createHidden("div", "edit evalVisible", "edit", bodyConst, "Edit").click(function() {editor();});
+		createTexted("div", "trash evalHidden", "trash", bodyConst, "Trash").click(function() {visualizeTrash();});
+		createHidden("div", "advance evalVisible", "advance", bodyConst, "Next").click(function() {advanceOneStep();});
+		createHidden("div", "multistep evalVisible", "multistep", bodyConst, "Multi-Step").click(function() {multiStep();});
+		createHidden("div", "run evalVisible", "run", bodyConst, "Run").click(function() {stepTillDone();});
 		createTexted("div", "block V palette", "if", sidebarConst, "If"); 
 		createTexted("div", "block V palette", "while", sidebarConst, "While"); 
 		createTexted("div", "block V palette", "var", sidebarConst, "Var"); 
 		createTexted("div", "block V palette", "stringliteral", sidebarConst, "String Literal"); 
 		createTexted("div", "block V palette", "worldcall", sidebarConst, "Call World"); 
 		createTexted("div", "block V palette", "assign", sidebarConst, "Assignment"); 
-		create("div", "userBar", "userBar", "body"); 
-		createTexted("div", "userOptions", "login", $("#userBar"), "Login/Register"); 
-		createTexted("div", "userOptions", "logout", $("#userBar"), "Logout").hide();
-		createTexted("div", "userOptions", "userSettings", $("#userBar"), "User Settings").hide();
-		createTexted("div", "userOptions", "saveProgram", $("#userBar"), "Save Program").hide();
-		createTexted("div", "userOptions", "loadProgram", $("#userBar"), "Load Program").hide();
+		create("div", "userBar", "userBar", bodyConst); 
+		const userBarConst = $("#userBar");
+		createTexted("div", "userOptions", "login", userBarConst, "Login/Register"); 
+		createTexted("div", "userOptions", "logout", userBarConst, "Logout").hide();
+		createTexted("div", "userOptions", "userSettings", userBarConst, "User Settings").hide();
+		createTexted("div", "userOptions", "saveProgram", userBarConst, "Save Program").hide();
+		createTexted("div", "userOptions", "loadProgram", userBarConst, "Load Program").hide();
 
         $('#login').click(function () {
-            $('body').append("<div id='dimScreen'></div>");
+            bodyConst.append("<div id='dimScreen'></div>");
             $('#dimScreen').append("<div id='registrationBox'>" +
                 "<div id='loginSection'>" +
                 "Login <br>" +
@@ -208,15 +180,15 @@ module mkHTML {
 
 		createTexted("div", "block V palette", "vardecl", sidebarConst, "Var Declaration"); 
 		createTexted("div", "block V palette", "lambda", sidebarConst, "Lambda Expression"); 
-		create("datalist", null, "oplist", "body"); 
+		create("datalist", null, "oplist", bodyConst); 
 
 		var optionList = ["+", "-", "*", "/", ">", "<", "==", ">=", "<=", "&", "|"];
 		for (var i = 0; i < optionList.length; i++) {
 			createValued("option", $("#oplist"), optionList[i]);
 		}
 
-		create("div", "container evalHidden", "container", "body"); 
-		createHidden("div", "vms evalVisible", "vms", "body", null); 
+		create("div", "container evalHidden", "container", bodyConst); 
+		createHidden("div", "vms evalVisible", "vms", bodyConst, null); 
 		create("div", null, "seq", $("#container")).attr("data-childNumber", "-1");
 		create("div", "dropZone H droppable", "dropZone", $("#seq")); 
 
@@ -280,6 +252,37 @@ module mkHTML {
         });
         enterBox();
     }
+
+	function create(elementType: string, className: string, idName: string, parentElement: JQuery) : JQuery {
+		var obj = $("<" + elementType + "></" + elementType + ">");
+		if (className) { obj.addClass(className); }
+		if (idName) { obj.attr("id", idName); }
+		if (parentElement) { obj.appendTo(parentElement); }
+		return obj;
+	}
+	
+	function createTexted(elementType: string, className: string, idName: string, parentElement: JQuery, textContent: string) : JQuery {
+		var obj = create(elementType, className, idName, parentElement);
+		if (textContent) { obj.text(textContent); }
+		return obj;
+	}
+
+	function createValued(elementType: string, parentElement: JQuery, value: string) : JQuery {
+		var obj = $("<" + elementType + "></" + elementType + ">");
+		if (parentElement) { obj.appendTo(parentElement); }
+		if (value) { obj.val(value); }
+		return obj;
+	}
+
+	function createPrepended(elementType: string, className: string, idName: string, parentElement: JQuery, textContent: string, prependToThis: JQuery) : JQuery {
+		var obj = createTexted(elementType, className, idName, parentElement, textContent);
+		if (prependToThis) { obj.prependTo(prependToThis); }
+		return obj;
+	}
+
+	function createHidden(elementType: string, className: string, idName: string, parentElement: JQuery, textContent: string) : JQuery {
+		return createTexted(elementType, className, idName, parentElement, textContent).css("visibility", "hidden");
+	}
 
     function redraw(vms:VMS) : void {
         turtleWorld.redraw() ;
