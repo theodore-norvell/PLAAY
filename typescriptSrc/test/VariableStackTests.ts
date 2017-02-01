827f56677e28524e0566b8d4b10e633c557519ee
Theodore Norvell <theo@mun.ca>
2017-01-31 22:52:35 -0330

    Renamed ExecStackTests.ts to VarStackTests.ts

--- typescriptSrc/test/VariableStackTests.ts
+++ typescriptSrc/test/VariableStackTests.ts
@@ -9,7 +9,6 @@
 
 import collections = require( '../collections' ) ;
 import assert = require( '../assert' ) ;
-import pnode = require( '../pnode' ) ;
 import valueTypes = require( '../valueTypes' ) ;
 import vms = require( '../vms' ) ;
 import world = require('../world') ;
@@ -17,36 +16,42 @@ import world = require('../world') ;
 import Evaluation = vms.Evaluation;
 import VMS = vms.VMS;
 import World = world.World;
-import varStrategy = pnode.varStrategy;
 import Field = valueTypes.Field;
+import ObjectV = valueTypes.ObjectV;
 import Type = vms.Type;
 import VarStack = vms.VarStack;
 
 var wrld : World = new World();
-var a : pnode.PNode = pnode.mkVar("a");
-var str = new valueTypes.StringV("");
-var f : Field = new Field(a.label().getVal(), str, Type.ANY , false);
 var xStack : VarStack = new VarStack(wrld, null);
 
 
-describe( 'Execution Stack ', () => {
+describe( 'Variable Stack ', () => {
 
     it('Should be initialized properly', () => {
         assert.check(xStack.getNext() == null);
         assert.check(xStack.obj == wrld);
-        assert.check(xStack.getField(f.getName()) == null);//already in the execution stack?
+        assert.check( xStack.inStack("+") );
+        assert.check( xStack.getField("+") != null);
     } );
 
     it('Should not find values not in the stack', () => {
         assert.check(!xStack.inStack("not in stack"));
-        assert.check(xStack.getField("not in stack") == null);
+        //assert.check(xStack.getField("not in stack") == null);
     } );
 
     it('Should be able to look up values', () => {
-        //inStack function fails
-        //assert.check(xStack.inStack(f.getName()));//broken only returns false for some reason
-       // assert.check(xStack.top().getField(f.getName()) == f);
-        assert.check(xStack.getField(f.getName()) == f);
-        //xStack.top().
+
+        var str = new valueTypes.StringV("");
+        var f : Field = new Field("abc", str, Type.ANY , false);
+        var obj : ObjectV = new ObjectV() ;
+        obj.addField( f ) ;
+        var yStack = new VarStack( obj, xStack) ;
+        assert.check( yStack.inStack(f.getName()));
+        assert.check( yStack.top().getField(f.getName()) == f );
+        assert.check( yStack.getField(f.getName()) == f );
+        assert.check( yStack.inStack("+") );
+        assert.check( yStack.getField("+") != null );
+        assert.check( ! yStack.inStack("not in stack") );
+        //assert.check( yStack.getField("not in stack") == null );
     } );
 } ) ;
