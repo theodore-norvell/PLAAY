a5bd9b59cb184599c3fa5e495206e008bde318d0
Theodore Norvell <theo@mun.ca>
2017-02-15 01:15:38 -0330

    Revert "Culled outdated file references (evaluationManager, stackManager) in interpretation."
    This reverts commit 3d221a1f69e9483db6bf01be4f025900263999c1.

--- typescriptSrc/interpretation.ts
+++ typescriptSrc/interpretation.ts
@@ -1,11 +1,15 @@
 ﻿/// <reference path="assert.ts" />
 /// <reference path="collections.ts" />
+/// <reference path="evaluation.ts" />
+/// <reference path="stackManager.ts" />
 /// <reference path="vms.ts" />
-/// <reference path="valueTypes.ts" />
+/// <reference path="value.ts" />
 
 import assert = require('./assert');
 import collections = require('./collections');
-import valueTypes = require('./valueTypes');
+import evaluation = require('./evaluation');
+import stack = require('./stackManager');
+import value = require('./value');
 import vms = require('./vms');
 
 module interpretation {
