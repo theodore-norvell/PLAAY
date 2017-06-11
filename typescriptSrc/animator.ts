c19e48d29436312d01a2a0083a813c8d696057ae
Theodore Norvell <theo@mun.ca>
2017-06-11 08:01:27 -0230

    Delinting editor and animator.

--- typescriptSrc/animator.ts
+++ typescriptSrc/animator.ts
@@ -30,7 +30,7 @@ import world = require('./world') ;
 module animator 
 {
     import EvaluationManager = evaluationManager.EvaluationManager;
-	import traverseAndBuild = sharedMkHtml.traverseAndBuild;
+    import traverseAndBuild = sharedMkHtml.traverseAndBuild;
     import List = collections.List;
     import arrayToList = collections.arrayToList;
     import ValueMap = vms.ValueMap;
@@ -40,12 +40,12 @@ module animator
     import Value = vms.Value ;
 
     const evaluationMgr = new EvaluationManager();
-    var turtle : boolean = false ;
-    var highlighted = false;
+    let turtle : boolean = false ;
+    let highlighted = false;
 
     const turtleWorld = new seymour.TurtleWorld();
 	
-	export function executingActions() 
+    export function executingActions() : void 
 	{
 		$("#play").click(evaluate);
 		$("#advance").click(advanceOneStep);
@@ -56,12 +56,12 @@ module animator
 
     function evaluate() : void
     {
-		$(".evalHidden").css("visibility", "hidden");
-		$(".evalVisible").css("visibility", "visible");
-        let libraries : valueTypes.ObjectV[] = [] ;
+        $(".evalHidden").css("visibility", "hidden");
+        $(".evalVisible").css("visibility", "visible");
+        const libraries : valueTypes.ObjectV[] = [] ;
         if( turtle ) libraries.push( new world.TurtleWorldObject(turtleWorld) ) ;
-        evaluationMgr.initialize(editor.getCurrentSelection().root(),
-            libraries );
+        evaluationMgr.initialize( editor.getCurrentSelection().root(),
+                                  libraries );
         $("#vms").empty()
 			.append(traverseAndBuild(evaluationMgr.getVMS().getRoot(), -1, true)) ;
         $(".dropZone").hide();
@@ -71,14 +71,14 @@ module animator
     function advanceOneStep() : void
     {
         evaluationMgr.next();
-		$("#stackVal").empty();
-		$("#vms").empty()
+        $("#stackVal").empty();
+        $("#vms").empty()
 			.append(traverseAndBuild(evaluationMgr.getVMS().getRoot(), -1, true)) ;
-		var root = $("#vms :first-child").get(0);
+        const root = $("#vms :first-child").get(0);
         if (!highlighted && evaluationMgr.getVMS().isReady() ) 
         {
             const vms : HTMLElement = document.getElementById("vms") as HTMLElement ;
-            var list = evaluationMgr.getVMS().getPending();
+            const list = evaluationMgr.getVMS().getPending();
             findInMap(root, evaluationMgr.getVMS().getValMap());
             highlight($(root), list);
             visualizeStack(evaluationMgr.getVMS().getStack());
@@ -96,7 +96,7 @@ module animator
         }
     }
 
-    function stepTillDone() 
+    function stepTillDone() : void 
 	{
         evaluationMgr.next();
         const STEPLIMIT = 10000 ;
@@ -104,16 +104,16 @@ module animator
         {
             evaluationMgr.next();
 		}
-		$("#vms").empty()
+        $("#vms").empty()
 			.append(traverseAndBuild(evaluationMgr.getVMS().getRoot(), -1, true)) ;
-		const root = $("#vms :first-child").get(0);
+        const root = $("#vms :first-child").get(0);
         const list : List<number>= evaluationMgr.getVMS().getPending();
         const map : ValueMap = evaluationMgr.getVMS().getValMap();
         findInMap(root, map);
         highlight($(root), list);
     }
 
-    function multiStep() 
+    function multiStep() : void
 	{
         $('#advance').trigger('click');
         $('#advance').trigger('click');
@@ -122,8 +122,8 @@ module animator
 
     function switchToEditor() : void
     {
-		$(".evalHidden").css("visibility", "visible");
-		$(".evalVisible").css("visibility", "hidden");
+        $(".evalHidden").css("visibility", "visible");
+        $(".evalVisible").css("visibility", "hidden");
         $(".dropZone").show();
         $(".dropZoneSmall").show();
     }
@@ -136,8 +136,8 @@ module animator
     {
         if(pending.isEmpty())
         {
-            var self = $(parent);
-            if(self.index() == 0) 
+            const self = $(parent);
+            if(self.index() === 0) 
             {
 				$("<div class='selected V'></div>").prependTo(self.parent());
 			}
@@ -149,12 +149,12 @@ module animator
         }
         else
         {
-            var child = $(parent);
+            const child = $(parent);
             if ( child.children('div[data-childNumber="' + pending.first() + '"]').length > 0 )
             {
-                var index = child.find('div[data-childNumber="' + pending.first() + '"]').index();
-                var check = pending.first();
-                if(index != check) {
+                const index = child.find('div[data-childNumber="' + pending.first() + '"]').index();
+                const check = pending.first();
+                if(index !== check) {
 					highlight(parent.children[index], pending.rest());
 				}
                 else 
@@ -171,16 +171,16 @@ module animator
 
     function findInMap(root : HTMLElement, valueMap : ValueMap) : void
     {
-        for( let e of valueMap.getEntries() ) {
+        for( const e of valueMap.getEntries() ) {
             setHTMLValue(root, e.getPath(), e.getValue());
         }
     }
 
     function visualizeStack( varStack : VarStack ) : void
     {
-        for( let frame of varStack.getAllFrames() ) {
+        for( const frame of varStack.getAllFrames() ) {
             for( let i = frame.numFields()-1 ; i >= 0 ; --i ) {
-                let field = frame.getFieldByNumber(i) ;
+                const field = frame.getFieldByNumber(i) ;
                 const name = field.getName() ;
                 // We need a better way to visualize values than just strings!
                 const valString = field.getValue().toString() ;
@@ -195,7 +195,7 @@ module animator
     {
         if(path.isEmpty())
         {
-            var self = $(root);
+            const self = $(root);
             // TODO. toString may not be the best function to call here,
             // since it could return any old crap that is not compatible with
             // HTML.
@@ -203,20 +203,26 @@ module animator
         }
         else
         {
-            var child = $(root);
+            const child = $(root);
             if ( child.children('div[data-childNumber="' + path.first() + '"]').length > 0 )
             {
-                var index = child.find('div[data-childNumber="' + path.first() + '"]').index();
-                var check = path.first();
-                if(index != check) {
-                    setHTMLValue(<HTMLElement>root.children[index], path.rest(), value);
+                const index = child.find('div[data-childNumber="' + path.first() + '"]').index();
+                const check = path.first();
+                if(index !== check) {
+                    setHTMLValue( root.children[index] as HTMLElement,
+                                  path.rest(),
+                                  value);
 				} else {
-                    setHTMLValue(<HTMLElement>root.children[check], path.rest(), value);
+                    setHTMLValue( root.children[check] as HTMLElement,
+                                  path.rest(),
+                                  value);
 				}
             }
             else
             {
-                setHTMLValue(<HTMLElement>root.children[path.first()], path, value);
+                setHTMLValue( root.children[path.first()] as HTMLElement,
+                              path,
+                              value);
             }
         }
     }
