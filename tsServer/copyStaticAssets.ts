941602a091699547586d314f6a9f824e72f116ed
Theodore Norvell <theo@mun.ca>
2018-08-08 15:31:37 -0230

    Added logo to web page.

--- tsServer/copyStaticAssets.ts
+++ tsServer/copyStaticAssets.ts
@@ -2,7 +2,8 @@ const shell = require("shelljs");
 
 shell.cp("-R", "../typescriptSrc/plaay-style.css", "dist/public/");
 shell.cp("-R", "../typescriptSrc/plaay.js", "dist/public/");
-shell.cp("-R", "../docs/drawings/favicon.ico", "dist/public/");
+shell.cp("-R", "../docs/drawings/logo-small.png", "dist/public/");
+shell.cp("-R", "../docs/drawings/faviconit/favicon.ico", "dist/public/");
 shell.cp("-R", "../docs/drawings/faviconit/favicon-192.png", "dist/public/");
 shell.cp("-R", "../docs/drawings/faviconit/favicon-160.png", "dist/public/");
 shell.cp("-R", "../docs/drawings/faviconit/favicon-96.png", "dist/public/");
