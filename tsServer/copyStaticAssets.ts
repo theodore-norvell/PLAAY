f6c2352f2fdabe1d633fdccaa89373294c1ebafe
Theodore Norvell <theo@mun.ca>
2018-08-08 14:36:45 -0230

    Created a logo. Added a favicon.

--- tsServer/copyStaticAssets.ts
+++ tsServer/copyStaticAssets.ts
@@ -2,3 +2,10 @@ const shell = require("shelljs");
 
 shell.cp("-R", "../typescriptSrc/plaay-style.css", "dist/public/");
 shell.cp("-R", "../typescriptSrc/plaay.js", "dist/public/");
+shell.cp("-R", "../docs/drawings/favicon.ico", "dist/public/");
+shell.cp("-R", "../docs/drawings/faviconit/favicon-192.png", "dist/public/");
+shell.cp("-R", "../docs/drawings/faviconit/favicon-160.png", "dist/public/");
+shell.cp("-R", "../docs/drawings/faviconit/favicon-96.png", "dist/public/");
+shell.cp("-R", "../docs/drawings/faviconit/favicon-64.png", "dist/public/");
+shell.cp("-R", "../docs/drawings/faviconit/favicon-32.png", "dist/public/");
+shell.cp("-R", "../docs/drawings/faviconit/favicon-16.png", "dist/public/");
