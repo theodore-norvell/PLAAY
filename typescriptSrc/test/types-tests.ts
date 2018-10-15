cfaf7fb6b7b170c8b379e2ca3c46f36f93f43a55
kamrulnz <kamrulnz@gmail.com>
2018-10-15 14:27:35 -0230

    Added tests for exbottom and caseBottom

--- typescriptSrc/test/types-tests.ts
+++ typescriptSrc/test/types-tests.ts
@@ -5,8 +5,10 @@
 
 import assert = require( '../assert' ) ;
 import collections = require( '../collections' ) ;
-import interpreter = require( '../interpreter' ) ;
 import types = require('../types') ;
+
+import some = collections.some ;
+import none = collections.none ;
 
 describe( "Type.toString()", function() : void {
     it( "should work for Bottom", function() : void {
@@ -20,5 +22,37 @@ describe( "Type.toString()", function() : void {
         const ty = types.TopType.theTopType ;
         const result = ty.toString() ;
         assert.checkEqual( "Top", result ) ;
+    } ) ;
+} ) ;
+
+
+
+describe( "Type.exBottom() and types.caseBottom", function() : void {
+    function f() { return some( 4 ) ; }
+
+    it( "deconstructor should return none if sent to the wrong type", function() : void {
+        const ty = types.TopType.theTopType  ;
+        const result = ty.exBottom( f ) ;
+        assert.check(  result.isEmpty() ) ;
+    } ) ;
+
+
+    it( "deconstructor should return some for the right type", function() : void {
+        const ty = types.BottomType.theBottomType  ;
+        const result = ty.exBottom( f ) ;
+        assert.checkEqual( 4, result.first() ) ;
+    } ) ;
+
+    it( "case function should return none if sent to the wrong type", function() : void {
+        const ty = types.TopType.theTopType  ;
+        const result = types.caseBottom( f )( ty )  ;
+        assert.check(  result.isEmpty() ) ;
+    } ) ;
+
+
+    it( "case function should return some for the right type", function() : void {
+        const ty = types.BottomType.theBottomType  ;
+        const result = types.caseBottom( f )( ty ) ;
+        assert.checkEqual( 4, result.first() ) ;
     } ) ;
 } ) ;
