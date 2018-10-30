35b37ec72bbc337bae440ab4da5e797b0959661d
Theodore Norvell <theo@mun.ca>
2018-10-30 15:19:32 -0230

    Added one more test for right bottom rule.

--- typescriptSrc/test/subtype-tests.ts
+++ typescriptSrc/test/subtype-tests.ts
@@ -44,7 +44,7 @@ describe( "leftBottomRule", function() : void {
         assert.checkEqual( 0, subgoals.length ) ;
     } ) ;
 
-    it( "should succeed for Bottom -- 2", function() : void {
+    it( "should succeed twice for two Bottoms", function() : void {
         const goal = { theta: [bottom, top, bottom], delta: [] } ;
         const result = leftBottomRule( goal ) ;
         // Expect one array of subgoals.
@@ -80,6 +80,35 @@ describe( "rightBottomRule", function() : void {
         // expect the left side to consist of 1 type
         assert.checkEqual( "", theta0.toString() ) ;
         assert.checkEqual( "Top", delta0.toString() ) ;
+    } ) ;
+
+    it( "should succeed twice for 2 bottoms", function() : void {
+        const goal = { theta: [], delta: [bottom, top, bottom] } ;
+        const result = rightBottomRule( goal ) ;
+        // Expect a list of two arrays of subgoals.
+        assert.checkEqual( 2, result.size() ) ;
+
+        // Check the first
+        const subgoals0 = result.first() ;
+        // Expect one subgoal in the array
+        assert.checkEqual( 1, subgoals0.length ) ;
+        const subgoal0 = subgoals0[0] ;
+        const theta0 = subgoal0.theta ;
+        const delta0 = subgoal0.delta ;
+        // expect the left side to consist of 1 type
+        assert.checkEqual( "", theta0.toString() ) ;
+        assert.checkEqual( "Top,Bottom", delta0.toString() ) ;
+
+        // Check the second
+        const subgoals1 = result.rest().first() ;
+        // Expect one subgoal in the array
+        assert.checkEqual( 1, subgoals1.length ) ;
+        const subgoal1 = subgoals1[0] ;
+        const theta1 = subgoal1.theta ;
+        const delta1 = subgoal1.delta ;
+        // expect the left side to consist of 1 type
+        assert.checkEqual( "", theta1.toString() ) ;
+        assert.checkEqual( "Bottom,Top", delta1.toString() ) ;
     } ) ;
 
     it( "should fail for top", function() : void {
